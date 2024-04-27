b2e690fef39ba0e5afd0f79998c4d336b5f92355
ProgSomel <somelahmed88@gmail.com>
2024-04-27 12:05:39 +0600

    get route based on id done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,14 @@ async function run() {
       }
       const cursor =  spotsCollection.find().sort(sortOptions);
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/spots/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id: new ObjectId(id)};
+      const result = await spotsCollection.findOne(query);
       res.send(result);
     })
 
